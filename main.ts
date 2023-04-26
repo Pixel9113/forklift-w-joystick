dec1ad8c3cdc4b3292874f069efd7d06e4d10c8e
Pixel9113 <122548027+Pixel9113@users.noreply.github.com>
2023-04-26 11:27:03 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,5 +18,32 @@ joystickbit.initJoystickBit()
 radio.setGroup(19)
 basic.showIcon(IconNames.Ghost)
 basic.forever(function () {
-	
+    if (joystickbit.getRockerValue(joystickbit.rockerType.Y) > 800) {
+        radio.sendNumber(1)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 200) {
+        radio.sendNumber(2)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.sendNumber(6)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P14)) {
+        radio.sendNumber(7)
+    } else {
+        radio.sendNumber(0)
+    }
+})
+basic.forever(function () {
+    if (action == 1) {
+        forward()
+    }
+    if (action == 2) {
+        backward()
+    }
+    if (action == 6) {
+        forkUp()
+    }
+    if (action == 7) {
+        forkDown()
+    }
+    if (action == 0) {
+        wuKong.stopAllMotor()
+    }
 })
